8af17fc257990a901794ae5497eb949db0d7ad4b
ubobbitt <43225045+ubobbitt@users.noreply.github.com>
2018-12-01 21:12:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,12 @@
 function details( movieid) {
     var vid;
     
-    $("#result").html("Results: Page " + pageNum + "<br><br>");
-                for (i = 0; i < 5; i++) {
-                    var page = "&startIndex=" + i * 5;
+    $("#result").html("Results: Page " + pageNum + "<hr noshade>");
+                for (i = 0; i < 10; i++) {
+                    var page = "&startIndex=" + i * 10;
                     var num = i + 1;
                     $("#result").append('<a href = "javascript:;" class="page" startIndex = ' + page + ' pageNum="' + num + '">' + (i + 1) + '</a> &nbsp&nbsp');
                 }
-
-                $(".page").click(function() {
-                    start = $(this).attr("startIndex");
-                    pageNum = $(this).attr("pageNum");
-                    getBooks();
-                });
-
-                $("#result").append("<br><br>");
     
     $.get("https://api.themoviedb.org/3/movie/"+movieid+"?api_key=9824d5d7ab77a02aa6e9cc1b18bab686&language=en-US",
         function(result){
